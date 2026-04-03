afb3fe8e3c068ab467ce47de0c90e0009bd14403
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-04-03 15:44:59 +0200

    NFC

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ if __name__ == "__main__":
 
             if result and result != "WAITING":
                 if result != last_uid:
-                    logging.info(f"New NFC tag detected: {result}\n")
+                    log.info(f"New NFC tag detected: {result}\n")
                     last_uid = result
                     handle_nfc_interaction(result)
-                    logging.info("\nWaiting for next NFC tag...\n")
+                    log.info("\nWaiting for next NFC tag...\n")
             elif result is None:
                 time.sleep(2) # C'est pour éviter '''UN PEU''' de spammer la console avec Waiting, on peut ajuster ce délai selon les besoins
 
@@ -60,40 +60,4 @@ if __name__ == "__main__":
             time.sleep(1) # Minimiser l'utilisation du CPU
 
     except KeyboardInterrupt:
-        logging.info("\nNFC reader stopped.")
-
-
-"""    init_db()
-    last_uid = None
-
-    try:
-        while True:
-            result = read_nfc_tag()
-            if result and result != "WAITING":
-                if result != last_uid:
-                    print(f"New NFC tag detected: {result}\n")
-                    last_uid = result
-                    handle_nfc_interaction(result)
-                    print("\nWaiting for next NFC tag...\n")
-            elif result is None:
-                time.sleep(2) # C'est pour éviter '''UN PEU''' de spammer la console avec Waiting, on peut ajuster ce délai selon les besoins
-            
-            if result == "WAITING":
-                last_uid = None # Réinitialiser last_uid quand la carte est retirée, cela permet de retapper la même carte.
-            time.sleep(1) # Minimiser l'utilisation du CPU
-    except KeyboardInterrupt:
-        print("\nExiting program.")"""
-
-
-"""if __name__ == "__main__":
-    last_uid = None
-
-    while True:
-        result = read_nfc_tag()
-        if result and result != "WAITING":
-            if result != last_uid:
-                print(f"New NFC tag detected: {result}\n")
-                last_uid = result
-        elif result is None:
-            time.sleep(2) # C'est pour éviter '''UN PEU''' de spammer la console avec Waiting, on peut ajuster ce délai selon les besoins
-        time.sleep(1) # Ceci est pour minimiser l'utilisation du CPU, on peut aussi ajuster ce délai selon les besoins"""
+        log.info("\nNFC reader stopped.")
