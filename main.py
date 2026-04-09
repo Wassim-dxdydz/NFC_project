c947344f4e5aa53d244b4e6ca8a26ce8d31ccf83
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-04-09 16:22:38 +0200

    small update

--- main.py
+++ main.py
@@ -5,31 +5,6 @@ import time
 
 log = logging.getLogger(__name__)
 
-def test():
-    init_db()
-
-    p = Product(
-        nfc_tag="TEST001",
-        name="Test",
-        description="test de l'ajout d'un produit",
-        production_date="2026-03-31",
-        expiration_date="2028-03-31",
-        other_infos="Lot: A123",
-        quantity=10
-    )
-
-    add_product(p)
-    print_product(p)
-
-    update_quantity("TEST001",10)
-    print_product(get_product("TEST001"))
-
-    restock_product("TEST001", 15)
-    print_product(get_product("TEST001"))    
-
-    print_product(get_product("GTEST001"))
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s',
